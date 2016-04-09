dc7333ba04b2fa183292f246c8691ecdc51d9f8a
melike <demirmelike187@gmail.com>
2016-04-09 17:49:52 +0300

    blog deayı için template ve view metodu eklendi

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ] #127.0.0.1.800 den sonra blogtaki url.py den hiç bir şey gelmezse viewsin altındaki postlist metodunu çağır
